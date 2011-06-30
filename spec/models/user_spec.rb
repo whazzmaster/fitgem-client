80ea02cb0e3cecf1e21f76c3aa2a7dbc4a5c76bc
Zachery Moneypenny <whazzmaster@gmail.com>
2011-06-30 12:08:23 -0500

    Removing notification spec files (will soon remove notifications resource or change heavily), add user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
 describe User do
-  
+  it "creates an empty fitbit_account when created" do
+    FitbitAccount.should_receive(:new)
+    User.create!( :password => 'meepmoop', :password_confirmation => 'meepmoop', :email => 'meep@moop.com' )
+  end
 end
