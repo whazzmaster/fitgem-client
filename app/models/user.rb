343b09dc6f0f3ffa13ef3acc755e343faea8e3a0
Zachery Moneypenny <whazzmaster@gmail.com>
2012-02-19 12:09:14 -0600

    Added convenience method to find if an account is linked with oauth yet

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   has_one :fitbit_account
-  
+
   after_create :create_fitbit_account
-  
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -10,9 +10,17 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
-  
+
+  def linked?
+    if self.fitbit_account.nil?
+      false
+    else
+      self.fitbit_account.verified?
+    end
+  end
+
   private
-  
+
   def create_fitbit_account
     self.fitbit_account = FitbitAccount.new
     save
