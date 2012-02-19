e8228d5cc9f7722e29ba434f49db6b843a23c624
Zachery Moneypenny <whazzmaster@gmail.com>
2012-02-19 12:11:11 -0600

    Added presenter infrastructure so we can use presenter objects in the views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,13 @@
 module ApplicationHelper
-  
+  def bootstrap_form_for(object, options = {}, &block)
+    options[:builder] = BootstrapFormBuilder
+    form_for(object, options, &block)
+  end
+
+  def present(object, klass = nil)
+    klass ||= "#{object.class}Presenter".constantize
+    presenter = klass.new(object, self)
+    yield presenter if block_given?
+    presenter
+  end
 end
---
+++ app/presenters/base_presenter.rb
@@ -0,0 +1,22 @@
+class BasePresenter
+  def initialize(object, template)
+    @object = object
+    @template = template
+  end
+
+private
+
+  def self.presents(name)
+    define_method(name) do
+      @object
+    end
+  end
+
+  def h
+    @template
+  end
+
+  def method_missing(*args, &block)
+    @template.send(*args, &block)
+  end
+end
