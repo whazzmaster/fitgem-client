b28f1dfa4a5ed4a054ff50503a99069795db8728
Zachery Moneypenny <whazzmaster@gmail.com>
2012-02-19 12:07:31 -0600

    New structure for site examples
      * Added new guide_controller to handle requests for the guide
      * Added view stubs
      * Updated routes to use <root>/guide/<guide-part> urls

---
+++ app/controllers/guide_controller.rb
@@ -0,0 +1,19 @@
+class GuideController < ApplicationController
+  def introduction
+  end
+
+  def getting_started
+  end
+
+  def using_fitgem
+  end
+
+  def fitbit_resources
+  end
+
+  def fitbit_subscriptions
+  end
+
+  def fitgem_on_rails
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 FitgemClient::Application.routes.draw do
 
+  devise_for :users, :controllers => { :registrations => "users/registrations" } do
+    get  '/login'  => "devise/sessions#new"
+    post '/login'  => 'devise/sessions#create',    :as => :user_session
+    get  '/logout' => 'devise/sessions#destroy'
+    get  '/signup' => "users/registrations#new"
+  end
+
   # Speciality routing for the OAuth login flow
   get "fitbit", :controller => :fitgem_oauth, :action => :index
   get "fitbit/index", :controller => :fitgem_oauth, :action => :index
@@ -7,8 +14,6 @@ FitgemClient::Application.routes.draw do
   get "fitbit/verify", :controller => :fitgem_oauth, :action => :verify
   get "fitbit/info", :controller => :fitgem_oauth, :action => :info
   post "fitbit/disconnect", :controller => :fitgem_oauth, :action => :disconnect
-
-  devise_for :users
 
   # Routing for the code examples
   get "examples", :controller => :examples, :action => :index
@@ -18,7 +23,16 @@ FitgemClient::Application.routes.draw do
     get "fitgem", :controller => :fitgem, :action => :index
   end
 
-  get "welcome/index"
+  scope "/guide" do
+    get "introduction", :controller => :guide, :action => :introduction
+    get "getting-started", :controller => :guide, :action => :getting_started, :as => :getting_started
+    get "using-fitgem", :controller => :guide, :action => :using_fitgem, :as => :using_fitgem
+    get "fitbit-resources", :controller => :guide, :action => :fitbit_resources, :as => :fitbit_resources
+    get "fitbit-subscriptions", :controller => :guide, :action => :fitbit_subscriptions, :as => :fitbit_subscriptions
+    get "fitgem-on-rails", :controller => :guide, :action => :fitgem_on_rails, :as => :fitgem_on_rails
+  end
+
+  # Miscellaneous pages
   get "about", :controller => :welcome, :action => :about
   get "blog", :controller => :welcome, :action => :blog
 
