7186fcbbdcf3ef0fcd5e24eb13e7bd99dacdf52f
Zachery Moneypenny <whazzmaster@gmail.com>
2012-02-19 12:10:21 -0600

    Resetting functionality on resources controller in prep for migration to new guide structure

--- app/controllers/examples/resources_controller.rb
+++ app/controllers/examples/resources_controller.rb
@@ -1,10 +1,4 @@
 class Examples::ResourcesController < ApplicationController
-
   def index
-    client = conditionally_create_client
-    unless client.nil?
-      @info = client.user_info['user']
-      @body_measurements = client.body_measurements_on_date('today')
-    end
   end
 end
