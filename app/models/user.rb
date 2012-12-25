e209db9000b06a8ef25baa80626badaa80c53d78
Zachery Moneypenny <whazzmaster@gmail.com>
2012-12-25 15:47:55 -0600

    Fixed typo in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :username, :email, :password, :password_confirmation, :provider, :uid
+  attr_accessible :username, :email, :password, :password_confirmation, :provider, :uid,
     :remember_me, :oauth_token, :oauth_secret
 
   validates :username, presence: true
