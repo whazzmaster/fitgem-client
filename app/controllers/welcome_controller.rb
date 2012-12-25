e1de04c026ae697e6f6c738d2faac2f54f7bf238
Zachery Moneypenny <whazzmaster@gmail.com>
2012-12-25 15:23:21 -0600

    Removed unnecessary client creation in index action

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,6 @@
 class WelcomeController < ApplicationController
 
   def index
-    client = conditionally_create_client
-    @info = client.user_info['user'] unless client.nil?
   end
 
   def about
