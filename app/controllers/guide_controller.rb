9065c9eb49f50dd745c1673c4e5777b7dcbd0bfa
Zachery Moneypenny <whazzmaster@gmail.com>
2013-04-08 21:18:34 -0500

    Break resources page into individually-focused pages
    * Updates links throughout the site
    * Fixed all of the single/double quote mismatches in the routes file
    * Added stubs asking for pull requests to the two areas (subscriptions and fitgem-on-rails) that aren't finished yet

--- app/controllers/guide_controller.rb
+++ app/controllers/guide_controller.rb
@@ -2,17 +2,23 @@ class GuideController < ApplicationController
   def introduction
   end
 
-  def getting_started
-  end
-
   def using_fitgem
   end
 
-  def fitbit_resources
+  def user_information
   end
 
-  def fitbit_subscriptions
+  def body_measurements
   end
+
+  def activities
+  end
+
+  def foods
+  end
+
+  #def subscriptions
+  #end
 
   def fitgem_on_rails
   end
