83bd0c35cd30560dce564aa7ad55cf950f836be6
Zachery Moneypenny <whazzmaster@gmail.com>
2013-04-11 20:05:02 -0500

    Redirect to referer path after signing in through Fitbit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def after_sign_in_path_for(resource_or_scope)
+    if request.env['omniauth.origin']
+      request.env['omniauth.origin']
+    end
+  end
 end
