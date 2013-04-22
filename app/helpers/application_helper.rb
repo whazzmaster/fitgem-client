9fb5366b7582be19b64492b96365cde2accf716f
Zachery Moneypenny <whazzmaster@gmail.com>
2013-04-21 21:07:09 -0500

    Remove present function; we never used it.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,11 +3,4 @@ module ApplicationHelper
     options[:builder] = BootstrapFormBuilder
     form_for(object, options, &block)
   end
-
-  def present(object, klass = nil)
-    klass ||= "#{object.class}Presenter".constantize
-    presenter = klass.new(object, self)
-    yield presenter if block_given?
-    presenter
-  end
 end
