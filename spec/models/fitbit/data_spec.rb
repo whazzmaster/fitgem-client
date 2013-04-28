050930371bdb72876c45f312203202fd8f185d9c
Zachery Moneypenny <whazzmaster@gmail.com>
2013-04-28 15:51:03 -0500

    Added Fitbit::Data specs

---
+++ spec/models/fitbit/data_spec.rb
@@ -0,0 +1,21 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+
+describe Fitbit::Data do
+  describe '.data_available?' do
+    before(:each) do
+      @user = double('User')
+    end
+
+    it 'returns true when the user is linked to fitbit via oauth' do
+      @user.stub(:linked?).and_return(true)
+      Fitbit::Data.new(@user).data_available?.should be_true
+    end
+
+    it 'returns false when the user is not linked to fitbit via oauth' do
+      @user.stub(:linked?).and_return(false)
+      Fitbit::Data.new(@user).data_available?.should be_false
+    end
+
+
+  end
+end
