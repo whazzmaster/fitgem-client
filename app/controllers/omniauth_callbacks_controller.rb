b5867be620409a3145adf5eedeb69f49e9d6821a
Zachery Moneypenny <whazzmaster@gmail.com>
2013-04-29 17:13:54 -0700

    Fix bug related to nonexistant signup_path route

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -6,7 +6,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect user
     else
       session["devise.user_attributes"] = user.attributes
-      redirect_to signup_path
+      redirect_to  new_user_registration_path
     end
   end
   alias_method :fitbit, :all
